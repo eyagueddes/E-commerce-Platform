6248444fcbf465c2703ec5c079d0caed49be97bf
eyagueddes <eyagueddes1@gmail.com>
2023-10-30 22:29:39 +0100

    RoleController

--- src/main/java/com/example/ecommerce_platform/controllers/RoleController.java
+++ src/main/java/com/example/ecommerce_platform/controllers/RoleController.java
@@ -1,4 +1,30 @@
 package com.example.ecommerce_platform.controllers;
 
+import com.example.ecommerce_platform.entities.Role;
+import com.example.ecommerce_platform.services.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController("role")
 public class RoleController {
+
+
+    @Autowired
+    private RoleService roleService;
+
+    @GetMapping("list")
+    public String listRole(Model model){
+        String roleList=roleService.listRoles(model);
+        return roleList;
+    }
+    @GetMapping("add")
+    public Map<String, String> addRole(String role){
+        Map<String, String> roles=roleService.addRole(role);
+        return roles;
+    }
 }
