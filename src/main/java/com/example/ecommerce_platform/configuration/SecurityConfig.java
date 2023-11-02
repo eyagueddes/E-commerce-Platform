9aa417712a35b481449e776bf66d5c0d2d605503
eyagueddes <eyagueddes1@gmail.com>
2023-11-02 21:03:44 +0100

    spring security

--- src/main/java/com/example/ecommerce_platform/configuration/SecurityConfig.java
+++ src/main/java/com/example/ecommerce_platform/configuration/SecurityConfig.java
@@ -1,19 +1,58 @@
 package com.example.ecommerce_platform.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
    // a "bean" is a reusable software component that is managed and instantiated by the Spring container.
     //When you mark a method with @Bean, it tells Spring that this method will return an object that should be registered as a bean in the Spring application context.
     // The object returned by the method will be managed by the Spring container, and you can use it in other parts of your application.
+     BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
-        BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
         return  bCryptPasswordEncoder;
+    }
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("{spring.queries.roles-query}")
+    private String rolesQuery;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()  // Use a JDBC-based authentication provider
+                .usersByUsernameQuery(usersQuery)  // Specify the SQL query to retrieve user details
+                .authoritiesByUsernameQuery(rolesQuery)  // Specify the SQL query to retrieve user roles
+                .dataSource(dataSource)  // Set the data source for the database
+                .passwordEncoder(bCryptPasswordEncoder);  // Use BCrypt to hash passwords
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll() // Allow access to certain paths without authentication
+                .anyRequest().authenticated() // All other requests require authentication
+                .and()
+                .httpBasic(); // Enable httpBasic login
+
+        // Optionally, you can add more configuration here (e.g., custom login page, logout handling, etc.)
     }
 
 }
